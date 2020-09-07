cb9ea34081037f316d156695b37c7d46a09eb427
encounter1105 <1459722320@qq.com>
2020-09-07 13:41:58 +0800

    update

---
+++ src/main/java/com/example/talent/sep/StreamAlerter.java
@@ -0,0 +1,15 @@
+package com.example.talent.sep;
+
+public class StreamAlerter {
+//    private RingBuffer ring;
+//    private Trie trie;
+
+    public StreamAlerter(String[] strings) {
+        // ...
+    }
+
+    public boolean query(char ch) {
+        // ...
+        return false;
+    }
+}
